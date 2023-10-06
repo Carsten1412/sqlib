31bc86578c14ea8c2e6a819f2d7320810f50407a
Carsten Brockhagen <email1234568@icloud.com>
2023-10-06 15:59:26 +0200

    sqlib.go bearbeitet

--- sqlib.go
+++ sqlib.go
@@ -1,3 +1,32 @@
 package sqlib
 
-type sqloib struct{}
+import (
+	"database/sql"
+	"errors"
+	"os"
+)
+
+type sqlib struct{}
+
+func CreateNewDatabase(filename string) error {
+
+	if checkifexists(filename) {
+		return errors.New("Datenbankdatei existiert schon ... ! Datenbank konnte nicht erstellt werden !")
+	} else {
+		db, err := sql.Open("sqlite3", filename)
+		if err != nil {
+			return errors.New("Fehler beim Erstellen der Datenbank ... !")
+		}
+		defer db.Close()
+	}
+
+}
+
+func checkifexists(filename string) bool {
+	_, err := os.Stat(filename)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
